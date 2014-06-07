de5e24bc45a391a78e8b2fe4ddeda162757dabb0
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-06-07 20:42:37 +0300

    all request specs passing

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,50 +1,62 @@
 require 'spec_helper'
 
 describe RequestsController do
+  before :all do
+    FactoryGirl.create(:user)
+  end
 
-  # describe "GET 'destroy'" do
-  #   it "returns http success" do
-  #     request = FactoryGirl.create(:request)
-  #     delete :destroy, id: request.id
-  #     response.should be_success
-  #   end
+  before :each do
+    ApplicationController.any_instance.stub(:current_user).and_return(User.first)
+  end
 
-  #   it "removes record" do
-  #     request = FactoryGirl.create(:request)
-  #     expect {
-  #       delete :destroy, id: request.id
-  #     }.to change{ Request.count }.by(-1)
-  #   end
-  # end
+  describe "Delete 'destroy'" do
+    it "returns http success" do
+      request = User.first.requests.create(FactoryGirl.attributes_for(:request))
+      delete :destroy, id: request.id
+      response.should be_success
+    end
 
-  # describe "GET 'create'" do
-  #   context 'valid params' do
-  #     it "returns http success" do
-  #       post :create, request: FactoryGirl.attributes_for(:request)
-  #       response.should be_success
-  #     end
-  #     it 'creates a new request' do
-  #       expect {
-  #         post :create, request: FactoryGirl.attributes_for(:request)
-  #       }.to change { Request.count }.by(1)
-  #     end
-  #   end 
-  # end
+    it "removes record" do
+      request = User.first.requests.create(FactoryGirl.attributes_for(:request))
+      expect {
+        delete :destroy, id: request.id
+      }.to change{ Request.count }.by(-1)
+    end
+  end
 
-  # describe "GET 'played'" do
-  #   let(:request) { FactoryGirl.create(:request) }
-  #   before(:each) { request }
+  describe "Post 'create'" do
+    context 'valid params' do
+      it "returns http success" do
+        post :create, request: FactoryGirl.attributes_for(:request)
+        response.should be_success
+      end
+      it 'creates a new request' do
+        expect {
+          post :create, request: FactoryGirl.attributes_for(:request)
+        }.to change { Request.count }.by(1)
+      end
+    end 
+  end
 
-  #   it "returns http success" do
-  #     patch :played, id: request.id
-  #     response.should be_success
-  #   end
+  describe "Patch 'played'" do
+    before :all do
+      @party = User.first.parties.create(FactoryGirl.attributes_for(:party))
+    end
 
-  #   it "returns http success" do
-  #     expect {
-  #       patch :played, id: request.id
-  #     }.to change { Request.count }.by(-1)
-  #   end
-  # end
+    it "returns http success" do
+      myRequest = User.first.requests.create(
+        FactoryGirl.attributes_for(:request).merge({party_id: @party.id}))
+      patch :played, id: myRequest.id
+      response.should be_success
+    end
+
+    it "removes request from list" do
+      myRequest = User.first.requests.create(
+        FactoryGirl.attributes_for(:request).merge({party_id: @party.id}))
+      expect {
+        patch :played, id: myRequest.id
+      }.to change { Request.count }.by(-1)
+    end
+  end
 
 end
