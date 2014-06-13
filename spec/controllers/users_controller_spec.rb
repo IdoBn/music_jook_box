664edd9ea329c67c3c4e746582321783b8f47c2f
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-06-13 23:17:22 +0300

    adding user controller specs #show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe UsersController do
+  before(:all) { FactoryGirl.create(:user) }
 
+  context 'GET show' do
+    before(:each) do
+      get :show, id: User.first.id
+    end
+
+    it { response.should be_success }
+    it { expect(assigns(:user)).to eq(User.first) }
+  end
 end
