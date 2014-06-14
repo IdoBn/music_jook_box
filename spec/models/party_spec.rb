44a2fbd84716904e48329976b2d344c538cdf74f
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-06-14 10:14:37 +0300

    adding specs to party model

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Party do
   it { should have_many(:requests) }
   it { should belong_to(:user) }
+
+  # geocoder
+  it { should respond_to(:latitude) }
+  it { should respond_to(:longitude) }
 end
